2ae77c6d683db94d9067faa5030776506f98c1d3
yzyhk904 <86816519+yzyhk904@users.noreply.github.com>
2022-01-15 18:58:41 +0900

    Add files via upload
    Added ro.audio.usb.period_us=4000 or 5600 to system.prop to improve audio quality.

--- customize.sh
+++ customize.sh
@@ -10,4 +10,5 @@ else
 /system/vendor/lib/libalsautils.so
 "
   rm -rf "$MODPATH/system/vendor/lib64"
+  sed -i 's/ro\.audio\.usb\.period_us=.*$/ro\.audio\.usb\.period_us=5600/' "$MODPATH/system.prop"
 fi
