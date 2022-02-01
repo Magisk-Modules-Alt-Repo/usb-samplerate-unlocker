4ffbbfd7ae125acfa02e558dae0e28dbf034cd38
yzyhk904 <86816519+yzyhk904@users.noreply.github.com>
2022-02-01 21:33:04 +0900

    Add files via upload
    Dummy empty libalsautils.so's were removed because some people worried about them.
    From now on, they will be generated through a Magisk installation process.

--- customize.sh
+++ customize.sh
@@ -1,14 +1,22 @@
-# Replace libalsautils.so's
+# Making dummies for replacing libalsautils.so's
+
+REPLACE=""
+for d in "/system/vendor/lib" "/system/vendor/lib64"; do
+    if [ -r "${d}/libalsautils.so" ]; then
+        mkdir -p "${MODPATH}${d}"
+        touch "${MODPATH}${d}/libalsautils.so"
+        chmod 644 "${MODPATH}${d}/libalsautils.so"
+        chmod -R a+rX "${MODPATH}${d}"
+        if [ -z "${REPLACE}" ]; then
+            REPLACE="${d}/libalsautils.so"
+        else
+            REPLACE="${REPLACE} ${d}/libalsautils.so"
+        fi
+    fi
+done
 
 if "$IS64BIT"; then
-  REPLACE="
-/system/vendor/lib/libalsautils.so
-/system/vendor/lib64/libalsautils.so
-"
+    :
 else
-  REPLACE="
-/system/vendor/lib/libalsautils.so
-"
-  rm -rf "$MODPATH/system/vendor/lib64"
-  sed -i 's/ro\.audio\.usb\.period_us=.*$/ro\.audio\.usb\.period_us=5600/' "$MODPATH/system.prop"
+    sed -i 's/ro\.audio\.usb\.period_us=.*$/ro\.audio\.usb\.period_us=5600/' "$MODPATH/system.prop"
 fi
--- functions2.sh
+++ functions2.sh
@@ -1,6 +1,7 @@
 #!/system/bin/sh
 
-function toHexLE() {
+function toHexLE()
+{
     if [ $# -eq 1  -a  $1 -gt 0 ]; then
       printf "%2.2x%2.2x%2.2x%2.2x" $(( $1 % 256 ))  $(( $1 / 256 % 256 ))  $(( $1 / 256 / 256 % 256 ))  $(( $1 / 256 / 256 / 256 % 256 ))
       return 0
@@ -9,7 +10,8 @@ function toHexLE() {
     fi
 }
 
-function toHexLineLE() {
+function toHexLineLE()
+{
     if [ $# -eq 1 ]; then
       local i
       for i in $1; do
@@ -23,8 +25,9 @@ function toHexLineLE() {
 
 # Patch libalsautils.so to clear the 96kHz lock of USB audio class drivers
 #   arg1: original libalsautils.so file;  arg2: patched libalsautils.so file; 
-#     optional arg3: "max" (clearing full sample rates), "full" (clearing upto 386kHz), "default" or others (clearing upto 192kHz)
-function patchClearLock() {
+#     optional arg3: "max" (clearing upto 768kHz), "full" (clearing upto 386kHz), "default" or others (clearing upto 192kHz)
+function patchClearLock()
+{
     local orig_rates='96000  88200  192000  176400  48000  44100  32000  24000  22050  16000  12000  11025  8000'
     local new_rates='192000  176400  96000  88200  48000  44100  32000  24000  22050  16000  12000  11025  8000'
     
@@ -67,7 +70,8 @@ function patchClearLock() {
 
 # Patch audio.primary.${boardname}.so file to clear the 384kHz lock of USB audio class offload drivers
 #   arg1: original audio.primary.${boardname}.so file;  arg2: patched audio.primary.${boardname}.so file; 
-function patchClearOffloadLock() {
+function patchClearOffloadLock()
+{
     local orig_rates='384000  352800  192000  176400  96000  88200  64000  48000  44100  32000  22050  16000  11025  8000'
     local new_rates='768000  705600  384000  352800  192000  176400  96000  88200  48000  44100  22050  16000  11025  8000'
     
